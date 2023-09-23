1f3659c010eba9b7ce8c87f6d8a9e858c10ea7fa
sasikumar-07 <128038363+sasikumar-07@users.noreply.github.com>
2023-09-23 09:25:43 +0530

    Delete pharmacy_management_system directory

--- pharmacy_management_system/src/main/java/com/example/demo/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.CustomerEntity;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Integer>{
-
-}
